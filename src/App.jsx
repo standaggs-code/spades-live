4221557bd1b88209936d6e32b9a4d226b5743137
standaggs-code <stan.daggs@gmail.com>
2026-05-02 21:03:11 -0500

    re-added lobby code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Lobby from './Lobby';
-import GameTable from './GameTable'; 
+import GameTable from './GameTable';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* The Root URL goes to the Lobby */}
         <Route path="/" element={<Lobby />} />
+        
+        {/* URLs with a room ID go to the Game Table */}
         <Route path="/room/:roomId" element={<GameTable />} />
       </Routes>
     </BrowserRouter>
